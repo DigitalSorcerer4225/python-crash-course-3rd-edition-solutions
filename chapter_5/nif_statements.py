ed2c888836f96027f811ff2ed15116bd2fdce753
Vince <92703060+DigitalSorcerer4225@users.noreply.github.com>
2026-06-12 15:34:48 +0800

    add more programming exercises

--- chapter_5/nif_statements.py
+++ chapter_5/nif_statements.py
@@ -127,3 +127,51 @@ if 'extra cheese' in requested_toppings:
     print("Adding extra cheese.") 
 
 print("\nFinished making your pizza!")
+
+
+# Using if Statements with Lists
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    print(f"Adding {requested_topping}")
+
+print("\nFinished making your pizza!")
+
+# Checking for Special Items: What if green peppers are unavailable?
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!\n")
+
+# Checking that a list is not Empty
+
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}).")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
+
+# Using Multiple lists to validate allowable inputs:
+
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+                    'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f'Adding {requested_topping}.')
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+    
+print("\nFinished making your pizza!")
