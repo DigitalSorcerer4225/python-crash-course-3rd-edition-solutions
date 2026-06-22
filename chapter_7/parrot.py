8e9f9ae01aeedb050a5baecff4e2ec67c7d3807f
Vince <92703060+DigitalSorcerer4225@users.noreply.github.com>
2026-06-22 16:12:28 +0800

    update chapter 7 exercises.py

--- chapter_7/parrot.py
+++ chapter_7/parrot.py
@@ -1,8 +1,22 @@
-# message = input('Tell me something, and I will repeat it back to you: ')
-# print(message)
+prompt = 'Tell me something, and I will repeat it back to you:'
+prompt += "\nEnter 'quit' to end the program. "
 
-prompt = "If you share your name, we can personalize the messages you see."
-prompt += "\nWhat is your first name? "
+message = ""
 
-name = input(prompt)
-print(f"\nHello, {name}!") 
+while message.lower() != 'quit':
+    message = input(prompt)
+    
+    # if message == 'quit':
+    #     print("")
+    # else:
+    #     print(f"{message}\n")
+
+    if message != 'quit':
+        print(f"{message}\n")
+
+# prompt = "If you share your name, we can personalize the messages you see."
+# prompt += "\nWhat is your first name? "
+
+# name = input(prompt)
+# print(f"\nHello, {name}!") 
+
