323d61e0b46b9f4f67916e4f49f90689b1203844
Vince <92703060+DigitalSorcerer4225@users.noreply.github.com>
2026-06-23 15:42:56 +0800

    delete chapter 7 files

--- counting_break.py
+++
@@ -1,9 +0,0 @@
-prompt = 'Tell me something, and I will repeat it back to you:'
-prompt += "\nEnter 'quit' to end the program. "
-
-while True:
-    message = input(prompt)
-    if message.lower() != 'quit':
-        print(f"{message}\n")
-    else:
-        break
--- restaurant_seating.py
+++
@@ -1,9 +0,0 @@
-# 7-2. Restaurant Seating: Write a program that asks the user how many people  are in their dinner group. If the answer is more than eight, print a message say­ ing they’ll have to wait for a table. Otherwise, report that their table is ready. 
-
-table = input("How many companions do you have for this dinner? ")
-table = int(table)
-
-if table > 8:
-    print("I'm sorry, you'll have to wait for a table.")
-else:
-    print("Certainly. Your table is now ready.")
